89f851c9ed950487209a3b768d54171b40232608
Middle Shell <denob311302@gmail.com>
2023-03-13 00:01:56 +0300

    StartWorkParticleFish|FreezeYCamera

--- Assets/_Game/Scripts/CameraController.cs
+++ Assets/_Game/Scripts/CameraController.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //[SerializeField] Vector2 offset = new Vector2(2f, 1f);
-    
     [Header("Parameters")]
     [SerializeField] private GameObject _player;
-    [SerializeField] [Range(0.5f, 7.5f)] private float movingSpeed = 5f;
+    [SerializeField] [Range(0.5f, 10f)] private float movingSpeed = 5f;
     [SerializeField] private Vector3 _offsetXYZ = new (4f, 0f, -500f);//-500 по Z что бы не было спрайтов уходящих за камеру
 
     void Awake()
@@ -20,6 +18,15 @@ public class CameraController : MonoBehaviour
     {
         Vector3 target = _player.transform.position + _offsetXYZ;
 
+        if (target.y > 1.85f)
+        {
+            target.y = 1.85f;
+        }
+        if (target.y < -1.85f)
+        {
+            target.y = -1.85f;
+        }
         transform.position = Vector3.Lerp(transform.position, target, movingSpeed * Time.deltaTime);
+        
     }
 }
