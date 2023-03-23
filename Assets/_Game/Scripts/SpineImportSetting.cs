70df895a555d216dca8270067700b8cb86985ba9
Middle Shell <denob311302@gmail.com>
2023-03-23 17:42:12 +0300

    ReloadSpinePack|Animations|

---
+++ Assets/_Game/Scripts/SpineImportSetting.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Spine;
+/// <summary>
+/// Modify the Spine file version
+/// </summary>
+public class SpineImportSetting : AssetPostprocessor
+{
+    // Any resource (including folder) imports will be called
+    void OnPreprocessAsset()
+    {
+        try
+        {
+
+            if (!this.assetPath.EndsWith(".json"))
+                return;
+               
+            // Judgment is a spine file
+            string msg = File.ReadAllText(this.assetPath, Encoding.UTF8);
+            JObject jo = JObject.Parse(msg);
+            string item = jo["skeleton"]["spine"].ToString();
+               
+            if (!string.IsNullOrEmpty(item)&& item.ToString()!="3.8")
+            {
+                jo["skeleton"]["spine"] = "3.8";// Modify version 3.8 version
+                File.WriteAllText(this.assetPath, jo.ToString());
+                AssetDatabase.Refresh();
+            }
+               
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("SpineImportSetting exception {e.Message}");
+        }
+    }
+  
+}
